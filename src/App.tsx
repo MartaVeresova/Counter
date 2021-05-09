f7b3d2ed8fee02b4eab473dfdd848d3d979c5bae
MartaVeresova <MartaVeresovaa@gmail.com>
2021-05-09 22:58:54 +0300

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Counter} from './Counter';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [number, setNumber] = useState(0)
+    const inc = () => {
+        setNumber(number + 1)
+    }
+    const reset = () => {
+        setNumber(0)
+    }
+
+    return (
+        <div>
+            <Counter
+                inc={inc}
+                reset={reset}
+                number={number}
+            />
+        </div>
+    );
 }
 
 export default App;
+
+
