af284f4939511c57442f78b3c35534ffc175281a
MartaVeresova <MartaVeresovaa@gmail.com>
2021-05-12 18:50:08 +0300

    App, Counter - add settings block, logics, css

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,70 @@
-import React, {useState} from 'react';
-import {Counter} from './Counter';
+import React, {useEffect, useState} from 'react';
+import {Counter, SettingBlock} from './Counter';
+import s from './App.module.css'
 
 function App() {
     const [number, setNumber] = useState(0)
+    const [startValue, setStartValue] = useState(0)
+    const [maxValue, setMaxValue] = useState(0)
+    const [errorMax, setErrorMax] = useState(false)
+    const [errorStart, setErrorStart] = useState(false)
+    const [editMode, setEditMode] = useState(false)
+
+
+    useEffect(() => {
+        let maxNewValue = localStorage.getItem('maxValue')
+        let startNewValue = localStorage.getItem('startValue')
+        if (maxNewValue) {
+            setMaxValue(JSON.parse(maxNewValue))
+        }
+        if (startNewValue) {
+            setStartValue(JSON.parse(startNewValue))
+            setNumber(JSON.parse(startNewValue))
+        } else {
+            setErrorMax(true)
+            setErrorStart(true)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+    }, [maxValue, startValue])
+
+
     const inc = () => {
         setNumber(number + 1)
     }
     const reset = () => {
-        setNumber(0)
+        setNumber(startValue)
     }
 
     return (
-        <div>
+        <div className={s.app}>
+            <SettingBlock
+                startValue={startValue}
+                setStartValue={setStartValue}
+                number={number}
+                setNumber={setNumber}
+                maxValue={maxValue}
+                setMaxValue={setMaxValue}
+                errorMax={errorMax}
+                setErrorMax={setErrorMax}
+                errorStart={errorStart}
+                setErrorStart={setErrorStart}
+                editMode={editMode}
+                setEditMode={setEditMode}
+            />
             <Counter
                 inc={inc}
                 reset={reset}
                 number={number}
+                startValue={startValue}
+                maxValue={maxValue}
+                errorMax={errorMax}
+                errorStart={errorStart}
+                editMode={editMode}
+                setEditMode={setEditMode}
             />
         </div>
     );
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,23 +1,137 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './App.module.css';
 
 export type CounterType = {
     inc: () => void
     reset: () => void
     number: number
+    startValue: number
+    maxValue: number
+    errorMax: boolean
+    errorStart: boolean
+    editMode: boolean
+    setEditMode: (editMode: boolean) => void
+}
+
+export type SettingBlockType = {
+    startValue: number
+    setStartValue: (value: number) => void
+    number: number
+    setNumber: (number: number) => void
+    maxValue: number
+    setMaxValue: (value: number) => void
+    errorMax: boolean
+    errorStart: boolean
+    setErrorMax: (errorMax: boolean) => void
+    setErrorStart: (errorStart: boolean) => void
+    editMode: boolean
+    setEditMode: (editMode: boolean) => void
+
 }
 
 export function Counter(props: CounterType) {
 
+    /*const errorMessage = props.error || props.startValue <= props.maxValue
+        ? <span>enter values and press set</span> //css-подобный объект; инлайновая стилизация
+        : props.error || (props.startValue < 0 || props.maxValue < 0)
+            ? <span style={{color: 'red'}}>Incorrect value!</span>
+            : <span className={props.number === props.maxValue ? `${s.number} ${s.maxNumber}` : `${s.number}`}>
+                {props.number}
+            </span>*/
+
     return (
         <div className={s.container}>
-            <span className={props.number === 5 ? `${s.number} ${s.maxNumber}` : `${s.number}`}>
-                {props.number}
+
+            <span>
+                {
+                    props.editMode && !props.errorMax && !props.errorStart
+                        ? <span className={`${s.number} ${s.editMode}`}>enter values and press set</span>
+                        : props.errorMax || props.errorStart
+                        ? <span className={`${s.number} ${s.error}`}>Incorrect value!</span>
+                        : !props.editMode && !props.errorMax && !props.errorStart
+                            ? <span
+                                className={props.number === props.maxValue ? `${s.number} ${s.maxNumber}` : `${s.number}`}>{props.number} </span>
+                            : null
+                }
             </span>
+
             <div className={s.buttons}>
-                    <button className={s.button} onClick={props.inc} disabled={props.number === 5}>inc</button>
-                    <button className={s.button} onClick={props.reset} disabled={!props.number}>reset</button>
+                <button className={s.button} onClick={props.inc}
+                        disabled={props.number === props.maxValue || props.editMode}>inc
+                </button>
+                <button className={s.button} onClick={props.reset} disabled={!props.number || props.errorStart || props.errorMax}>reset
+                </button>
             </div>
+        </div>
+    )
+}
+
+export function SettingBlock(props: SettingBlockType) {
+
+    const onChangeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+        props.setEditMode(true)
+        props.setMaxValue(e.currentTarget.valueAsNumber)
+        if (e.currentTarget.valueAsNumber < 0 || e.currentTarget.valueAsNumber < props.startValue) {
+            props.setErrorMax(true)
+            props.setErrorStart(false)
+        } else if (e.currentTarget.valueAsNumber === props.startValue) {
+            props.setErrorMax(true)
+            props.setErrorStart(true)
+        } else if (e.currentTarget.valueAsNumber > 0 && props.startValue === 0) {
+            props.setErrorMax(false)
+            props.setErrorStart(false)
+        } else if (e.currentTarget.valueAsNumber > props.startValue) {
+            props.setErrorMax(false)
+            props.setErrorStart(false)
+        } else props.setErrorMax(false)
+    }
+
+    const onChangeStartValue = (e: ChangeEvent<HTMLInputElement>) => {
+        props.setEditMode(true)
+        props.setStartValue(e.currentTarget.valueAsNumber)
+        if (e.currentTarget.valueAsNumber < 0 || e.currentTarget.valueAsNumber > props.maxValue) {
+            props.setErrorStart(true)
+            props.setErrorMax(false)
+        } else if (e.currentTarget.valueAsNumber === props.maxValue) {
+            props.setErrorStart(true)
+            props.setErrorMax(true)
+        } else if (e.currentTarget.valueAsNumber === 0 && e.currentTarget.valueAsNumber < props.maxValue) {
+            props.setErrorStart(false)
+            props.setErrorMax(false)
+        } else if (e.currentTarget.valueAsNumber < props.maxValue) {
+            props.setErrorStart(false)
+            props.setErrorMax(false)
+        } else props.setErrorStart(false)
+    }
+
+    const onClickButtonSet = () => {
+        props.setNumber(props.startValue)
+        props.setEditMode(!props.editMode)
+    }
+
+    return (
+        <div className={s.container}>
+            <div className={s.number}>
+                <div className={s.input}>
+                    {/*<span>max value:</span>*/}
+                    <input
+                        className={props.errorMax ? `${s.input} ${s.errorInput}` : s.input}
+                        type="number"
+                        value={props.maxValue}
+                        onChange={onChangeMaxValue}
+                    />
+                    {/*<span>start value:</span>*/}
+                    <input
+                        className={props.errorStart ? `${s.input} ${s.errorInput}` : s.input}
+                        type="number"
+                        value={props.startValue}
+                        onChange={onChangeStartValue}
+                    />
+                </div>
+            </div>
+            <button className={s.button} onClick={onClickButtonSet} disabled={props.errorMax || props.errorStart}>
+                set
+            </button>
         </div>
     )
 }
